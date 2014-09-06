a4a9deae0aeb90b3c55d9a014a7627df8611232c
cs3343JSONXML@gmail.com <cs3343JSONXML@gmail.com@46fbf642-335a-63ab-ffb2-144ecf634341>
2014-09-06 14:17:44 +0000

    Prototype v2

--- trunk/JsonXmlTranslator/src/component/Translator.java
+++ trunk/JsonXmlTranslator/src/component/Translator.java
@@ -2,6 +2,6 @@ package component;
 
 public class Translator {
 	public static void main(String args[]){
-		System.out.println("Hello, I am the translator.");
+		System.out.println("Hello everyone, I am the translator.");
 	}
 }
