41cf4ccd0db00038d5a134136f05730be900bca3
cs3343JSONXML@gmail.com <cs3343JSONXML@gmail.com@46fbf642-335a-63ab-ffb2-144ecf634341>
2014-09-07 10:39:44 +0000

    jason testing commit

--- trunk/JsonXmlTranslator/src/component/Translator.java
+++ trunk/JsonXmlTranslator/src/component/Translator.java
@@ -3,5 +3,6 @@ package component;
 public class Translator {
 	public static void main(String args[]){
 		System.out.println("Hello everyone, I am the translator.");
+		System.out.println("Hello, Jason testing");
 	}
 }
