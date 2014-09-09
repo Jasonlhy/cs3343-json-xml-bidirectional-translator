73f26485990add36b2f2031eb65ed1d407a9243c
cs3343jsonxml <cs3343JSONXML@gmail.com>
2014-09-09 15:56:27 +0000

    Winson testing commit

--- JsonXmlTranslator/src/component/Translator.java
+++ JsonXmlTranslator/src/component/Translator.java
@@ -4,5 +4,6 @@ public class Translator {
 	public static void main(String args[]){
 		System.out.println("Hello everyone, I am the translator.");
 		System.out.println("Hello, Jason testing");
+		System.out.println("Hello, Winson testing");
 	}
 }
