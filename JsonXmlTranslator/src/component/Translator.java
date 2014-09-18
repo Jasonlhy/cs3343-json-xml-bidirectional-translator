a4a1779db0f98c4a44900aa15fb40ffc1aa29aaa
khtlee3343 <khtlee3343@gmail.com>
2014-09-18 05:25:23 +0000

    logging feature

--- JsonXmlTranslator/src/component/Translator.java
+++ JsonXmlTranslator/src/component/Translator.java
@@ -1,11 +1,13 @@
 package component;
 
+import utility.log.CustomLog;
+
 public class Translator {
 	public static void main(String args[]){
 		System.out.println("Hello everyone, I am the translator.");
 		System.out.println("Hello, Jason testing");
 		System.out.println("Hello, Winson testing");
 		System.out.println("Hello, Chihang testing");
-		System.out.println("Hello, Thomas testing.");
+		System.out.println("Hello, Thomas testing");
 	}
 }
