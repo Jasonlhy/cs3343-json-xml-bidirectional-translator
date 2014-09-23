e3e59a966f0a06a724d0736f98333264a2ce4754
chunyin112@hotmail.com <chunyin112@hotmail.com@46fbf642-335a-63ab-ffb2-144ecf634341>
2014-09-23 10:45:24 +0000

    Object that implement the reader interface

---
+++ trunk/JsonXmlTranslator/src/utility/io/FatTommyFileReader.java
@@ -0,0 +1,40 @@
+package utility.io;
+
+import java.io.*;
+import java.nio.file.Files;
+
+
+public class FatTommyFileReader implements TommyFileReader{
+	
+	private String filename;
+	
+	
+	private FatTommyFileReader(){
+		
+	}
+	
+	public FatTommyFileReader(String filename){
+		this();
+		this.filename = filename;
+	}
+	
+	public BufferedReader getFileReader() throws FileNotFoundException{
+		InputStream in  = new FileInputStream(filename);
+		return  new BufferedReader(new InputStreamReader(in));
+		
+	}
+	
+	public String readWholeFile() throws IOException{
+		String line ;
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = this.getFileReader();
+		
+		while((line=br.readLine())!=null){
+			sb.append(line);
+			
+		}
+		
+		return sb.toString();
+	}
+
+}
