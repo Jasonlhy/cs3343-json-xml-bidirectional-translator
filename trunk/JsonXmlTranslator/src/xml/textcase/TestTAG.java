3bbb8e32e04017556bbe06ad496741257088b1ec
khtlee3343@gmail.com <khtlee3343@gmail.com@46fbf642-335a-63ab-ffb2-144ecf634341>
2014-09-24 13:34:27 +0000

    Finish 6.1.1 task tag validator

---
+++ trunk/JsonXmlTranslator/src/xml/textcase/TestTAG.java
@@ -0,0 +1,53 @@
+package xml.textcase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import xml.TAG;
+
+public class TestTAG {
+
+	@Test
+	public void testValidOpenTag() {
+		String test = "<open>";
+		boolean result = TAG.OPEN.isValid(test);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testInvalidOpenTag() {
+		String test = "blablabla";
+		boolean result = TAG.OPEN.isValid(test);
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testValidCloseTag() {
+		String test = "</close>";
+		boolean result = TAG.CLOSE.isValid(test);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testInvalidCloseTag() {
+		String test = "blablabla";
+		boolean result = TAG.CLOSE.isValid(test);
+		assertFalse(result);
+	}
+	
+	@Test
+	public void testValidShortTag() {
+		String test = "<short/>";
+		boolean result = TAG.SHORT.isValid(test);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testInvalidShortTag() {
+		String test = "blablabla";
+		boolean result = TAG.SHORT.isValid(test);
+		assertFalse(result);
+	}
+
+}
