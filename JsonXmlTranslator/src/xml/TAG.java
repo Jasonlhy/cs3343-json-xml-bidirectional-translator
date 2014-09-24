755bd1aab4b2c57d9072865839ceff6a7e31bcc7
khtlee3343 <khtlee3343@gmail.com>
2014-09-24 13:34:27 +0000

    Finish 6.1.1 task tag validator

---
+++ JsonXmlTranslator/src/xml/TAG.java
@@ -0,0 +1,28 @@
+package xml;
+
+import java.util.regex.Pattern;
+
+/**
+ * enum class that declare xml tag patterns
+ * @author Thomas
+ *
+ */
+public enum TAG { 
+	OPEN("<(?!/).+(?<!/)>"), CLOSE("</.+>"), SHORT("<.+/>");
+	private String regex;
+
+	private TAG(String regex){
+		this.regex = regex;
+	}
+	
+	/**
+	 * Return if the input str is a valid tag (OPEN, CLOSE or SHORT)
+	 * 
+	 * @param str input to test
+	 * @return true if it is valid, false if it is invalid
+	 */
+	public boolean isValid(String str){
+		return Pattern.compile(this.regex).matcher(str).matches();
+	}
+}
+
