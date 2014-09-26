42d570db94ed1ebc773ab16731188bfabc6c6e88
chunyin112 <chunyin112@hotmail.com>
2014-09-26 06:03:13 +0000

    Add JUnit TestCase for FatTommyFileReader

---
+++ JsonXmlTranslator/src/utility/io/TestFatTommyFileReader.java
@@ -0,0 +1,14 @@
+package utility.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFatTommyFileReader {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
