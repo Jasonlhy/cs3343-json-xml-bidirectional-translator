aae3f419cbeddd6e09c56567543b4b3eb97d8afa
chunyin112@hotmail.com <chunyin112@hotmail.com@46fbf642-335a-63ab-ffb2-144ecf634341>
2014-10-02 10:27:05 +0000

    Add testcase for utility.io fix:remove all the spaces within a line

---
+++ trunk/JsonXmlTranslator/src/utility/io/testcase/TestFatTommyFileReader.java
@@ -0,0 +1,36 @@
+package utility.io.testcase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import utility.io.FatTommyFileReader;
+
+import org.junit.Test;
+
+
+
+public class TestFatTommyFileReader {
+
+
+	@Test
+	public void testReadStudent() throws IOException{
+		FatTommyFileReader r = new FatTommyFileReader("./XmlFile/Student.txt");
+		assertEquals("<student></student>",r.readWholeFile());
+	}
+	
+	
+	@Test
+	public void testInvalidStudent() throws IOException{
+		FatTommyFileReader r = new FatTommyFileReader("./XmlFile/InvalidStudent.txt");
+		assertEquals("<student>",r.readWholeFile());
+	}
+	
+	
+	@Test
+	public void testStudentWithName() throws IOException{
+		FatTommyFileReader r = new FatTommyFileReader("./XmlFile/StudentWithName.txt");
+		assertEquals("<student><name>Tommy</name></student>",r.readWholeFile());
+	}
+
+}
