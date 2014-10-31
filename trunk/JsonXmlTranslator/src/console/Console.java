54f167b60f0a93d955f25bea602780eb2fcca932
Jasonlhy@gmail.com <Jasonlhy@gmail.com@46fbf642-335a-63ab-ffb2-144ecf634341>
2014-10-31 07:29:58 +0000

    1. Try to integrate the components with the console program 2. Right now it is only output translation result to the console

--- trunk/JsonXmlTranslator/src/console/Console.java
+++ trunk/JsonXmlTranslator/src/console/Console.java
@@ -1,6 +1,14 @@
 package console;
 
+import java.io.IOException;
 import java.util.Scanner;
+
+import component.JSONParser;
+import component.Node;
+import component.XMLWriter;
+import utility.io.FatTommyFileReader;
+import xml.NodeToXml;
+import xml.XmlToNode;
 
 public class Console {
 	String transformOption;
@@ -25,14 +33,64 @@ public class Console {
 	
 	public void transformOption() {
 		Scanner scanner = new Scanner(System.in);
-		transformOption = scanner.next();
-		if(transformOption.equals("X") || transformOption.equals("XML"))
-			System.out.print("X");
-		else if (transformOption.equals("J")|| transformOption.equals("JSON"))
-			System.out.print("J");
+		transformOption = scanner.nextLine();
+		
+		
+		if(transformOption.equals("X") || transformOption.equals("XML")){
+			//System.out.print("X");
+			FatTommyFileReader r;
+			System.out.println("Please enter the xml file location");
+			String xmlFileLocation = scanner.nextLine();
+			System.out.println("Please enter the destination file location");
+			String jsonFileLocation = scanner.nextLine(); // not used right now
+			
+			try {
+				r = new FatTommyFileReader(xmlFileLocation);
+				String xmlContent = r.readWholeFile();
+				System.out.println("xml content: "+ xmlContent);
+				XmlToNode xmlToNode = new XmlToNode();
+				Node root = xmlToNode.Translate(xmlContent);
+				System.out.println("root: " + root);
+				XMLWriter xmlWriter = new XMLWriter();
+				xmlWriter.writeXML(root);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if (transformOption.equals("J")|| transformOption.equals("JSON")){
+			//System.out.print("J");
+			FatTommyFileReader r;
+			System.out.println("Please enter the json file location");
+			String jsonFileLocation = scanner.nextLine();
+			System.out.println("Please enter the xml file location");
+			String xmlFileLocation = scanner.nextLine(); // not used right now
+			
+			try {
+				r = new FatTommyFileReader(jsonFileLocation);
+				String jsonContent = r.readWholeFile();
+				//System.out.println("json content: "+ jsonContent);
+				JSONParser parser = new JSONParser(jsonContent);
+				Node root = parser.parse();
+				//System.out.println("-------- root: -------" + root);
+				NodeToXml nodeToXML = new NodeToXml();
+				nodeToXML.outputXMLFile(root);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		else{
 			System.out.print("Please choose the translation option either XML or JSON !");
 			transformOption=null;
 		}
+		scanner.close();
+	}
+	
+	public static void main(String [] args){
+		Console console = new Console();
+		console.welcomeMessage();
+		console.transformOptionMessage();
+		console.transformOption();
 	}
 }
