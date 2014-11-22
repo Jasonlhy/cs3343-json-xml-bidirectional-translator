54e0a855f3dca34c416209c645a1a57c74fdbcdf
Jasonlhy <jasonlhy@gmail.com>
2014-11-22 10:55:07 +0000

    refine some code structure

--- JsonXmlTranslator/src/json/JSONBeautifier.java
+++ JsonXmlTranslator/src/json/JSONBeautifier.java
@@ -65,19 +65,20 @@ public class JSONBeautifier {
 		String eachLine = "";
 		for (int idx = 0; idx < jsonString.length(); idx++) {
 			char c = jsonString.charAt(idx);
+			
 			if (c == '{') {
 				intentedString += eachLine.trim();
-				intentLevel += 1;
-				intentedString += getNextLineCharacter(c, intentLevel);
+				intentLevel    += 1;
+				intentedString += c + "\n" + createIntent(intentLevel); 
 				eachLine = "";
 			} else if (c == '}') {
 				intentedString += eachLine.trim();
-				intentLevel -= 1;
-				intentedString += getNextLineCharacter(c, intentLevel);
+				intentLevel    -= 1;
+				intentedString += "\n" + createIntent(intentLevel) + c;
 				eachLine = "";
 			} else if (c == ',') {
 				intentedString += eachLine.trim();
-				intentedString += getNextLineCharacter(c, intentLevel);
+				intentedString += c + "\n" + createIntent(intentLevel);
 				eachLine = "";
 			} else {
 				// Skip line break, tab... etc
@@ -102,41 +103,7 @@ public class JSONBeautifier {
 		return (asiiNumber >= 32);
 	}
 
-	/**
-	 * Get character following end line character and next line intent
-	 * 
-	 * @param theChar
-	 *            The character
-	 * @param intentLevel
-	 *            the intent level
-	 * @return a beautiful string
-	 */
-	public String getNextLineCharacter(char theChar, int intentLevel) {
-		String outputString = "";
-		// for open bracket and common:
-		// print the char at current level
-		// then next char is at next level with intent
-		//
-		// for close bracket:
-		// print the end line character
-		// print the char at next level with intent
-		//
-		// for normal character:
-		// just print the char
-		if (theChar == '{' || theChar == ',') {
-			outputString += theChar;
-			outputString += "\n";
-			outputString += createIntent(intentLevel);
-		} else if (theChar == '}') {
-			outputString += "\n";
-			outputString += createIntent(intentLevel);
-			outputString += theChar;
-		} else {
-			outputString += theChar;
-		}
-
-		return outputString;
-	}
+	
 
 	/**
 	 * Create intent
@@ -152,19 +119,6 @@ public class JSONBeautifier {
 		}
 
 		return s;
-	}
-
-	public static void main(String[] args) {
-		JSONBeautifier b = new JSONBeautifier();
-		String r;
-		// r = b.beautifierUseString("{ \r\n id : 19, home : fanling }");
-		r = b.beautifierUseString("{\r\nid: 你好}");
-		System.out.println(r);
-
-		// r = b.endLineCharacter('\r');
-		// r =
-		// b.beautifier("{ id : 19, home : fanling, wife: { name : hehe, phonenumber : 61556960 }, uni : city }");
-		// System.out.println(r);
 	}
 
 }
