3c0e1c525e64b2805e5bac8000c97c22a46f8c76
jasonlhy@gmail.com <jasonlhy@gmail.com@46fbf642-335a-63ab-ffb2-144ecf634341>
2014-11-29 09:44:05 +0000

    Add javadoc into string reader, writer.

--- trunk/JsonXmlTranslator/src/utility/StringFileWriter.java
+++ trunk/JsonXmlTranslator/src/utility/StringFileWriter.java
@@ -6,23 +6,44 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 
+/**
+ * Concrete class that implements StringFileWriterInterface for writing a list of string into a file
+ */
 public class StringFileWriter implements StringFileWriterInterface{
 
 	
+	/** The file path of output file*/
 	String outPath ;
 	
+	/**
+	 * Instantiates a new string file writer by specifying the output file
+	 *
+	 * @param filePath the file path of the output file
+	 */
 	public StringFileWriter(String filePath){
 		outPath = filePath;
 		
 	}
+	
+	/**
+	 * Instantiates a new string file writer by specifying the default file to "./result.txt"
+	 */
 	public StringFileWriter(){
 		outPath = "./result.txt";
 	}
 	
+	/**
+	 * Sets the output path.
+	 *
+	 * @param filePath the file path
+	 */
 	public void SetOutputPath(String filePath){
 		this.outPath=filePath;
 	}
 	
+	/* (non-Javadoc)
+	 * @see utility.StringFileWriterInterface#WriteToFile(java.util.List)
+	 */
 	public void WriteToFile(List<String> input) throws IOException{
 		
 		if(input!=null){
